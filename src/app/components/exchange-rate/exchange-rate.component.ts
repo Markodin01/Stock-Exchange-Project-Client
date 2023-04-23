a6bff59c1067e5b53640e4eaba0ebdf11674495a
Marek Gajewski <marek.gajewski@logiblox.com>
2023-04-23 01:04:25 +0100

    comments

--- src/app/components/exchange-rate/exchange-rate.component.ts
+++ src/app/components/exchange-rate/exchange-rate.component.ts
@@ -24,17 +24,24 @@ export class ExchangeRateComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // This function checks if a given date is a weekend day (Saturday or Sunday).
+  // It returns true if it's not a weekend day, false otherwise.
   isWeekend = (date: Date): boolean => {
     const day = date.getDay();
-    // Prevent Saturday and Sunday from being selected.
     return day !== 0 && day !== 6;
   }
 
+  // This function sends an HTTP GET request to the NBP API to get the average exchange rate
+  // for a given currency and date. It sets the 'averageRate' variable to the returned value.
   getExchangeRate() {
     this.apiService.getAverageExchangeRate(this.currency, this.date).subscribe((data: any) => {
       this.averageRate = data.rate;
     });
   }
+
+  // This function sends an HTTP GET request to the NBP API to get the maximum and minimum exchange rates
+  // for a given currency and number of records. It sets the 'minMax' variable to a string representation of
+  // the maximum and minimum rates separated by a plus sign.
   getMinMax() {
     if (this.number !== null) {
       this.apiService.getMinMax(this.currency, this.number).subscribe((data: any) => {
@@ -42,7 +49,9 @@ export class ExchangeRateComponent implements OnInit {
       });
     }
   }
-  
+
+  // This function sends an HTTP GET request to the NBP API to get the major difference between the exchange
+  // rates for a given currency and number of records. It sets the 'majorDiff' variable to the returned value.
   getMajorDiff() {
     if (this.number !== null){
       this.apiService.getMajorDifference(this.currency, this.number).subscribe((data: any) => {
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,26 +5,23 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ApiService {
+  // Set the base URL for the API
   private baseUrl = 'https://nbp-api.uhddbtuebceh6.eu-west-2.cs.amazonlightsail.com';
 
   constructor(private http: HttpClient) { }
 
+  // Get the average exchange rate for a given currency and date
   getAverageExchangeRate(currency: string, date: string) {
     return this.http.get(`${this.baseUrl}/exchanges/${currency}/${date}`);
   }
 
+  // Get the minimum and maximum exchange rates for a given currency and number of records
   getMinMax(currency: string, num_records: number) {
     return this.http.get(`${this.baseUrl}/exchanges/${currency}/max-min/${num_records}`);
   }
 
+  // Get the records with the biggest differences in exchange rates for a given currency and number of records
   getMajorDifference(currency: string, num_records: number) {
     return this.http.get(`${this.baseUrl}/exchanges/${currency}/major-difference/${num_records}`);
   }
 }
-
-
-
-
-
-
-
